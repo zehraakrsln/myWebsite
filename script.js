b1d72d9f9fca9580a034ea7d33e895448a9de0fb
zehraakarslan <zehraakarslan02@gmail.com>
2025-04-24 17:49:36 +0300

    yeni sayfalar eklendi

---
+++ script.js
@@ -0,0 +1,17 @@
+function navigateToPage(pageUrl) {
+    window.location.href = pageUrl; // Sayfayı yönlendirme
+}
+// Menü açma fonksiyonu
+function showMenu() {
+    document.getElementById("nav-links").style.right = "0";
+}
+
+// Menü kapama fonksiyonu
+function hideMenu() {
+    document.getElementById("nav-links").style.right = "-200px";
+}
+
+// Sayfa yönlendirme fonksiyonu
+function navigateToPage(pageUrl) {
+    window.location.href = pageUrl; // Sayfayı yönlendirme
+}
